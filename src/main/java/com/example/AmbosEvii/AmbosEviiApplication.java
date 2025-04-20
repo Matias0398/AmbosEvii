ec74d4383e78c44fcc5f0b5d56c27cfb87aaf063
Matias <matias.llanos1998@gmail.com>
2025-04-20 19:41:17 -0300

    Primer commit del proyecto

---
+++ src/main/java/com/example/AmbosEvii/AmbosEviiApplication.java
@@ -0,0 +1,13 @@
+package com.example.AmbosEvii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmbosEviiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmbosEviiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AmbosEvii/AmbosEviiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AmbosEvii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmbosEviiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
